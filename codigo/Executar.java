e86a89b2619d8debfb128d48970ba33b820631e1
André Mendes <afmendes@sga.pucminas.br>
2024-06-22 04:16:59 -0300

    Update Executar.java

--- codigo/Executar.java
+++ codigo/Executar.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class Executar {
     public static void main(String[] args) {
         int quantidadeDeEnergia = Menu.quantidadeDeEnergia();
         Menu.ListaDeInteressadas();
         Interessadas.mostrarInteressadas();
+        ArrayList<Interessada> ListaDeInteressadas = Interessadas.retornaInteressadas();
+        Backtracking.backtrack(ListaDeInteressadas, quantidadeDeEnergia);
     }
 }
