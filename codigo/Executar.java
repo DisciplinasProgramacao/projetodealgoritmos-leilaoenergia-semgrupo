b285787ebd68102301d4b2cb37dac30eadb7efb1
André Mendes <afmendes@sga.pucminas.br>
2024-06-23 15:18:56 -0300

    Update Executar.java

--- codigo/Executar.java
+++ codigo/Executar.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class Executar {
     public static void main(String[] args) {
-        int quantidadeDeEnergia = Menu.quantidadeDeEnergia();
-        Menu.listaDeInteressadas();
+        int quantidadeDeEnergia = 8000;
+        ProcessarEntrada.tratarDadosEntrada();
+        //Menu.listaDeInteressadas();
         System.out.println("Quantidade de Energia: "+ quantidadeDeEnergia);
         ArrayList<Interessada> listaDeInteressadas = Interessadas.retornaInteressadas();
         Interessadas.mostrarInteressadas();
